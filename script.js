718d7fa4ef06641141ae393dfd0e87fe4caeefa1
Eswar Chandu <eswar_chandu@srmap.edu.in>
2022-11-17 11:22:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonClicked(){
+    alert('Page Loaded .....JS');
+}
+
+function buttonToChangeTextClicked(){
+    document.getElementById('targetTag').innerHTML="Java Script Text"
+}
+
+function htmltextChange(){
+    document.getElementById('targettag').innerHTML="My HTML CONTENT using JavaScript "
+}
